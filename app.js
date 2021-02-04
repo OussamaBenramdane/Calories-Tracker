5774f1197ad7e554e91be3e16803fada678ab270
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2021-02-04 02:19:35 +0100

    5th commit

--- app.js
+++ app.js
@@ -51,6 +51,22 @@ const ItemCtrl = (function(){
             });
             return found ;
         },
+        updateItem : function (name , calories) {
+            //Calorie to number
+            calories = parseInt(calories);
+            
+
+            let found = null;
+            data.items.forEach(function(item){
+                if(item.id === data.currentItem.id){
+
+                 item.name   = name ;
+                 item.calories = calories;
+                 found =item ;
+                }
+            });
+            return found;
+        },
         setCurrentItem:function(item){
             data.currentItem = item ;
         },
@@ -85,6 +101,7 @@ const ItemCtrl = (function(){
 const UICtrl = (function(){
     //Selector
     const UiSelectors={
+        listItems:'#item-list  li',
         itemList :'#item-list',
         addBtn : '.add-btn',
         updateBtn : '.update-btn',
@@ -132,10 +149,26 @@ const UICtrl = (function(){
             <em>${item.calories} Calories</em>
             <a href="#" class="secondary-content">
                 <i class="edit-item fa fa-pencil"></i>
-            </a>`
+            </a>`;
             //Insert item
             document.querySelector(UiSelectors.itemList).insertAdjacentElement('beforeend',li)
 
+        },
+        updateListItem: function(item){
+            let listItems = document.querySelectorAll(UiSelectors.listItems);
+            //Turn Node list into array
+            listItems = Array.from(listItems);
+            listItems.forEach(function(listItem){
+                const itemID = listItem.getAttribute('id');
+                if(itemID === `item-${item.id}`){
+                    document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name} : </strong>
+                    <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>` ;
+                }
+            });
+            
         },
         clearInput : function(){
             //Clearing the input
@@ -187,8 +220,19 @@ const App = (function(ItemCtrl,UICtrl){
         //Add Item Event
         document.querySelector(UISelectors.addBtn).addEventListener('click',itemAddSubmit);
 
+        // DIsable submit on enter
+        document.addEventListener('keypress',function (e) {
+            if(e.keyCode === 13 || e.which === 13){
+                e.preventDefault();
+                return false;
+            }
+        });
+
         //Edit icon click event 
-        document.querySelector(UISelectors.itemList).addEventListener('click',itemUpdateClick);
+        document.querySelector(UISelectors.itemList).addEventListener('click',itemEditClick);
+
+        //Update item event
+        document.querySelector(UISelectors.updateBtn).addEventListener('click',itemUpdateSubmit);
     }
     //Add item submit
     const itemAddSubmit = function(e){
@@ -213,7 +257,7 @@ const App = (function(ItemCtrl,UICtrl){
         e.preventDefault();
     }
     //Upadate item submit 
-    const itemUpdateClick = function(e){
+    const itemEditClick = function(e){
 
             if(e.target.classList.contains('edit-item')){
                 // Get lidt item id 
@@ -232,6 +276,23 @@ const App = (function(ItemCtrl,UICtrl){
 
             }
             e.preventDefault()
+    }
+    const itemUpdateSubmit = function(e){
+        // Get item input
+        const input = UICtrl.getTemInput();
+        //update item
+
+        const updatedItem = ItemCtrl.updateItem(input.name , input.calories);
+
+        // Update Ui 
+        UICtrl.updateListItem(updatedItem);
+        //Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        //Add total calories to the UI
+        UICtrl.showTotalCalories(totalCalories);
+        UICtrl.clearEditState();
+
+        e.preventDefault();
     }
     //Public methodes
 
